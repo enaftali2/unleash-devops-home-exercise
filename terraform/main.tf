8c3daddf47c8c105f462547548c48af4ae12b19d
enaftali2 <erannaf1@gmail.com>
2025-02-02 21:41:33 +0200

    eks access pub

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
 
 terraform {
@@ -27,7 +27,7 @@ resource "aws_subnet" "public_subnets" {
   count             = 2
   vpc_id           = aws_vpc.app_vpc.id
   cidr_block       = cidrsubnet(aws_vpc.app_vpc.cidr_block, 8, count.index)
-  availability_zone = element(["${var.region}a", "${var.region}b"], count.index)
+  availability_zone = element(["us-east-1a", "us-east-1b"], count.index)
 }
 
 resource "aws_security_group" "app_sg" {
@@ -41,10 +41,33 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
+resource "aws_security_group" "github_runner_sg" {
+  name        = "github_runner_sg"
+  description = "Allow SSH and other necessary traffic"
+  vpc_id      = aws_vpc.app_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["62.56.147.93/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "home-exercise-eks"
   cluster_version = "1.27"
   vpc_id          = aws_vpc.app_vpc.id
   subnet_ids      = aws_subnet.public_subnets[*].id
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,3 @@
-variable "project" {
- type        = string
- default     = "microsoft-exercise"
- description = "The GCP project name."
-}
-
 variable "region" {
  type        = string
  default     = "us-east-1"
